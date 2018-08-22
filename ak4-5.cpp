97dbec06bdd7c7fbbb927b48adabf7045073453d
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-08-22 19:36:59 +0530

    Add files via upload

---
+++ ak4-5.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+float p, r, t, si;
+cout << "convert p,r,t in si"<<endl;
+cout << "enter p";
+cin >>p;
+cout << "enter r";
+cin >> r;
+cout << "t";
+cin >> t;
+si = p*r*t/100;
+cout << "simple interest ="<<si<<endl;
+return 0;
+}
+
